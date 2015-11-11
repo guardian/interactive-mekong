ffe3cf0b26d496f259f10c743781939fb7d8a873
feilding <feilding.cage@gmail.com>
2015-11-11 14:09:28 +0000

    adds image loading for mobile, cuts images

--- src/utils/imageLoader.js
+++ src/utils/imageLoader.js
@@ -1,9 +1,32 @@
+var windowWidth,
+	imageSize,
+	imageSizes = [380, 450, 600];
+
+function init(){
+	windowWidth = window.innerWidth;
+
+	for(var s = 0; s < imageSizes.length; s ++){
+		if(imageSizes[s] >= windowWidth){
+			imageSize = imageSizes[s];
+			break;
+		} else if( s == imageSizes.length - 1){
+			imageSize = imageSize[s];
+		}
+	}
+
+}
 
 function loadImage(el, cardData, isMobile){
 
+	if(!imageSize){
+		init();
+	}
+
 	if(isMobile){
 		//load image for mobile
-		el.querySelector('.photo-bg-container').style.backgroundImage = 'url(' + cardData.mobile_url + ')';
+		var url = cardData.mobile_url;
+		url = url.slice( 0, url.lastIndexOf(".")) + '_' + imageSize + url.slice( url.lastIndexOf(".") , url.length);
+		el.querySelector('.photo-bg-container').style.backgroundImage = 'url(' + url + ')';
 	} else {
 		//load image for desktop
 		var url = cardData.photo_url;
