73b1916ae195f15a069a7bd8a1952b3a1535a6bc
feilding <feilding.cage@gmail.com>
2015-11-24 15:39:13 +0000

    adds tracking code

--- src/utils/tracker.js
+++ src/utils/tracker.js
@@ -1,13 +1,25 @@
 var cardsTracked = {};
 
 function track(cardId){
-	
-	if(!(cardId in cardsTracked)){
-		//track card
-		cardsTracked[cardId] = true;
-		//console.log(cardId)
+	if(window.s_gi){
+		//if tracking is available
+		if(!(cardId in cardsTracked)){
+			//track card
+			cardsTracked[cardId] = true;
+			var track_event = 'gv-card-' + cardId.replace('_', '-');
+
+			var s=s_gi('guardiangu-network');
+			s.linkTrackVars='eVar37,events';
+			s.linkTrackEvents='event37';
+			s.eVar37= track_event;
+			s.events='event37';
+			s.tl(true,'o', track_event);
+			console.log('event fired', track_event);
+		}
 
 	}
+
+
 
 }
 
